f2e2de348a0d3fa6b4794108886118091a5c5196
Ankit Thakur <ankit.t@nimbuspost.com>
2026-06-24 15:04:46 +0530

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/about', function () {
+    echo "About Us Page";
+});
+Route::get('/contact', function () {
+    echo "Contact Us page";
+});
+
